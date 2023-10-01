ab88fa2b5fd865163353dde7a973a98c72633df9
minty1202 <aki.men1202@gmail.com>
2023-10-01 20:21:35 +0900

    feat: add res

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
+import { useState } from 'react'
+
 function App() {
+  const [shifts, setShifts] = useState({})
+
+  const handlePost = async () => {
+    const res = await fetch('http://localhost:5000/api/v1/optimize', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        shifts: 'test',
+        staffs: 'test',
+      })
+    })
+    const data = await res.json()
+    setShifts(data)
+  }
 
   return (
     <>
-      Hello World!
+      <button onClick={handlePost}>post</button>
+      <div>{JSON.stringify(shifts)}</div>
     </>
   )
 }
